6b3413543c0d1d226b3147d2044c72db0a914bc0
Kostas Bariotis <konmpar@gmail.com>
2017-12-29 00:28:35 +0200

    add a few commnets

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,8 @@
+/* Require jQuery as a dep for Bootstrap JS */
+window.jQuery = window.$ = require('./../../node_modules/jquery');
+
+/* Require selective bootstrap components */
 require('./../../node_modules/bootstrap-sass/assets/javascripts/bootstrap/collapse')
 
+/* Require your custom CSS, let Parcel do the rest */
 require('./../scss/boot.scss')
